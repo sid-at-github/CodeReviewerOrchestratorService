36d2550d067c8271ddb5c6cc10762f3dac4956d9
Siddharth Papreja <sidd.papreja@gmail.com>
2019-11-15 16:59:23 +0000

    conneted to reviewee service

--- src/main/java/team/dexter/CodeReviewerOrchestrationService/main/App.java
+++ src/main/java/team/dexter/CodeReviewerOrchestrationService/main/App.java
@@ -2,13 +2,16 @@ package team.dexter.CodeReviewerOrchestrationService.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
+@ComponentScan(basePackages = { "team.dexter.CodeReviewerOrchestrationService.resources" })
 public class App {
+
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
--- src/main/java/team/dexter/CodeReviewerOrchestrationService/resources/RevieweeResource.java
+++ src/main/java/team/dexter/CodeReviewerOrchestrationService/resources/RevieweeResource.java
@@ -1,43 +1,19 @@
 package team.dexter.CodeReviewerOrchestrationService.resources;
 
-import java.io.IOException;
-
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import team.dexter.CodeReviewerOrchestrationService.dtos.RevieweeRequestDto;
 
 @RestController
 public class RevieweeResource {
 
-	@PostMapping("/reviewee")
-	public void createReviewee(@RequestBody RevieweeRequestDto info) throws ClientProtocolException, IOException {
-		CloseableHttpClient httpClient = HttpClients.createDefault();
-		HttpGet request = new HttpGet("https://www.google.com/search?q=mkyong");
-
-		try (CloseableHttpResponse response = httpClient.execute(request)) {
-			// Get HttpResponse Status
-			System.out.println(response.getStatusLine().toString());
-
-			HttpEntity entity = response.getEntity();
-			Header headers = entity.getContentType();
-			System.out.println(headers);
-
-			if (entity != null) {
-				// return it as a String
-				String result = EntityUtils.toString(entity);
-				System.out.println(result);
-			}
-
-		}
+	@PostMapping("/quotations")
+	public String createReviewee(@RequestBody RevieweeRequestDto info) {
+		RestTemplate restTemplate = new RestTemplate();
+		String result = restTemplate.getForObject("http://localhost:9001/reviewee", String.class);
+		return result;
 	}
 }
